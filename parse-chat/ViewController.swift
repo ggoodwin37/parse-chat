b879bad6141e1f65c70e547d4b789fc9e0369f4e
gideong <gideong@yahoo-inc.com>
2015-09-09 15:21:43 -0700

    Initial Commit

---
+++ parse-chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  parse-chat
+//
+//  Created by Gideon Goodwin on 9/9/15.
+//  Copyright © 2015 Gideon Goodwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
